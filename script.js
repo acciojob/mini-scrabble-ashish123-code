819455ec8b4550217c187c0c51c893003c0507dd
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-01-11 19:13:49 +0530

    mini scrabble

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //your code here
-const textbox = document.getElementById("evaluatedText");
-const letterCount = document.getElementById("letterCount");
 
-textbox.addEventListener("input", function() {
-  letterCount.innerHTML = textbox.value.length;
-});
+ // get the elements we will be working with
+ const textbox = document.getElementById('evaluatedText');
+ const letterCount = document.getElementById('letterCount');
+
+ // listen for changes to the textbox
+ textbox.addEventListener('input', updateLetterCount);
+
+ function updateLetterCount() {
+     // update the letter count to the length of the text in the textbox
+     letterCount.innerHTML = textbox.value.length;
+ }
 
